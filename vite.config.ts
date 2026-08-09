46d496dc53b7ef7a02a6d39dfb26de001b72cc8a
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-08-09 20:19:12 +0100

    refactor: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-import { copyFileSync } from "node:fs";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    {
-      name: "emit-ui-spa-not-found",
-      apply: "build",
-      closeBundle: () => copyFileSync("dist/index.html", "dist/404.html"),
-    },
-  ],
-});
+export default defineConfig({});
